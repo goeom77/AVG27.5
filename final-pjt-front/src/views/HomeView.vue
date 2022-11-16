a3d3f83904a16f4918fdbe202f9e1677f2e8d2ca
goeom77 <7eom14@gmail.com>
2022-11-16 11:41:22 +0900

    base

---
+++ final-pjt-front/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-link :to="{ name: 'ArticleView' }">Articles</router-link> | 
+    <router-link :to="{ name: 'SignUpView' }">SignUpPage</router-link> | 
+    <router-link :to="{ name: 'LogInView' }">LogInPage</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<style>
+
+</style>
